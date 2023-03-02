c4bc1a39679862623b6b93f6caeafd374d4fa324
Ogabek-Mustafoev <mustafoev0806@gmail.com>
2023-03-02 22:58:54 +0500

    env-removed

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,8 +1,8 @@
-const key = process.env.TMDB_KEY;
+// const key = process.env.TMDB_KEY;
 
 const apiConfig = {
   baseUrl: 'https://api.themoviedb.org/3/',
-  apiKey: key,
+  apiKey: 'c76eace6be6b9fd3296ab11da38b3e3c',
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
